f90474c96afec1ea569d6f128b1b623c597f511e
Justin Dennison <justin1dennison@gmail.com>
2018-02-18 18:22:49 -0500

    extracted UserList and UserItem components from the UserSideBar component

--- src/UserSideBar.js
+++ src/UserSideBar.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
 import './UserSideBar.css';
 
+class UserItem extends Component {
+    render(){
+        const {user, selectedUser, onSelect } = this.props;
+        const classes = selectedUser === user ? 
+                            'UserSideBar--list-item active' : 
+                            'UserSideBar--list-item';
+        return (<li className={classes}>
+                    <button onClick={() => onSelect(user)}>
+                        {user}
+                    </button>
+                </li>);
+    }
+}
+class UserList extends Component {
+    render(){
+        const { users, selectedUser, onSelect,  } = this.props;
+        const Users = users.map((user, index) => <UserItem 
+                                                        key={index}
+                                                        user={user}
+                                                        onSelect={onSelect}
+                                                        selectedUser={selectedUser}
+                                                        />)
+        return (
+            <ul className='UserSideBar--list'>
+                {Users}
+            </ul>
+        );
+    }
+}
 class UserSideBar extends Component {
     render(){
         const { users, selectedUser, onSelect } = this.props;
-        const Users = users.map((user, index) =>{
-            const classes = selectedUser === user ? 
-                            'UserSideBar--list-item active' : 
-                            'UserSideBar--list-item';
-            return (<li key={index} 
-                        className={classes}
-                        >
-                        <button onClick={() => onSelect(user)}>
-                            {user}
-                        </button>
-                    </li>);
-        })
+        
         return (
             <div className="UserSideBar--container">
                 <h1 className='UserSideBar--header'>GitHub Users</h1>
-                <ul className='UserSideBar--list'>
-                    {Users}
-                </ul> 
+                <UserList 
+                    users={users}
+                    selectedUser={selectedUser}
+                    onSelect={onSelect}/>
                 <div className="UserSideBar--user-form">
                     <form onSubmit={(e) => this.props.addUser(e, this.props.username)}>
                         <input type="text" 
